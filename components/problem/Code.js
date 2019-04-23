40c97a625baa48dd762c00082f9ce8d7ce49171a
josh <postor@gmail.com>
2019-04-23 20:50:27 +0800

    调整接口，未完待续

--- components/problem/Code.js
+++ components/problem/Code.js
@@ -1,40 +1,78 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { debounce } from 'throttle-debounce'
-import updateProblem from '../../lib/update-problem'
+import request from '../../lib/request'
 
 const SAVE_THROTTLE = 5 * 1000   //5sec
+const deboncedSaveCode = debounce(SAVE_THROTTLE, saveCode)
+const languages = ['nodejs', 'java']
 
 const Code = ({ problem, submitCode }) => {
+  const [language, setLanguage] = useState('nodejs')
   const [code, setCode] = useState('')
-  const ref = useRef()
+  const [codeId, setCodeId] = useState('')
   const { _id } = problem
+
   useEffect(() => {
-    //didMount
-    let { currentAnswer, codes } = problem
-    if (!currentAnswer) currentAnswer = codes
-    setCode(currentAnswer)
-    ref.current = debounce(SAVE_THROTTLE, saveCode)
-  }, [])
+    const { codes = {} } = problem
+    loadCode(language, _id).then(obj => {
+      if (obj) {
+        setCode(obj.code)
+        setCodeId(obj._id)
+        return
+      }
+      setCode(codes[language])
+      createCode({
+        code: codes[language],
+        problemId: _id,
+        language,
+      }).then(({ _id }) => setCodeId(_id))
+    })
+  }, [language])
+
   return (<div>
+    <div className="input-field col s12">
+      <select
+        style={{ display: 'block' }}
+        value={language}
+        onChange={(e) => setLanguage(e.target.value)}
+      >{languages.map((lang, i) => (<option key={i} value={lang}>{lang}</option>))}</select>
+    </div>
+    <hr />
     <textarea
       style={{
         width: '100%',
         minHeight: '400px'
       }}
-      value={code}
+      value={code || ''}
       onChange={e => {
         setCode(e.target.value)
-        ref.current(_id, e.target.value)
+        const obj = {
+          _id: codeId,
+          code: e.target.value,
+        }
+        deboncedSaveCode(obj)
       }} />
     <hr />
     <a className="waves-effect waves-light btn"
-      onClick={() => submitCode(code)}
+      onClick={() => submitCode(code, language)}
     >submit</a>
   </div>)
 }
 
 export default Code
 
-function saveCode(_id, currentAnswer) {
-  return updateProblem(_id, { currentAnswer })
+async function saveCode(obj) {
+  await request.post('/codes/update', obj)
+}
+
+async function createCode(obj) {
+  let r = await request.post('/codes/create', obj);
+  return r.data
+}
+
+async function loadCode(language, problemId) {
+  const r = await request.post('/codes/find', {
+    query: { problemId, language }
+  })
+  return r.data && r.data[0]
 }
